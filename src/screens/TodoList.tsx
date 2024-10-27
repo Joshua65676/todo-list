3ca02c8d9a87e834664c0054d473825e12c0fbf9
Okoro Joshua <Joshua65676@gmail.com>
2024-10-27 17:17:40 +0100

    first native commit

--- src/screens/TodoList.tsx
+++ src/screens/TodoList.tsx
@@ -108,86 +108,94 @@ const TodoList: React.FC = () => {
     </View>;
   }
 
-  return (
-    <View className="max-w-md mx-auto">
-      <View className="p-4">
-        <View className="flex flex-row justify-between">
-          <View className="pl-5">
-            <Text className="mb-1 font-medium text-md">Today</Text>
-            <Text className="mb-4 text-xl font-bold">{getCurrentDate()}</Text>
+
+  const renderHeader = () => (
+    <View className="p-4">
+      <View className="flex flex-row justify-between">
+        <View className="pl-5">
+          <Text className="mb-1 font-medium text-md">Today</Text>
+          <Text className="mb-4 text-xl font-bold">{getCurrentDate()}</Text>
+        </View>
+        <View>
+          <Pressable onPress={() => {
+            Alert.alert(
+              'Alert',
+              'You are trying to Log out, Would you like to continue?',
+              [
+                { text: 'Cancel', onPress: () => null },
+                { text: 'Ok', onPress: () => handleLogout() }
+              ]
+            );
+          }}>
+            <Text>LogOut</Text>
+          </Pressable>
+        </View>
+      </View>
+      <View className="h-32 p-4 pl-5 mt-3 bg-blue-400 rounded-xl">
+        <Text className="mb-2 text-xl font-bold text-white">
+          Welcome, {userName}!
+        </Text>
+        <Text className="text-base font-semibold text-white">
+          Keep it up!! Complete your tasks. You almost there!
+        </Text>
+      </View>
+    </View>
+  );
+
+  const renderFooter = () => (
+    <View className="max-w-md p-4 pt-1 pb-8 mx-auto">
+      <Pressable
+        className="absolute w-16 p-4 bg-blue-400 rounded-full shadow-lg bottom-28 left-32"
+        onPress={() => setModalVisible(true)}
+      >
+        <Text className="pl-2 text-2xl text-white ">+</Text>
+      </Pressable>
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        presentationStyle="pageSheet"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View className="items-center justify-center flex-1 bg-opacity-50">
+          <View className="mb-10">
+            <Text className="text-2xl font-bold">New Task</Text>
           </View>
-          <View>
-            <Pressable className="" onPress={() => {
-              Alert.alert(
-                 'Alert',
-                 'You are trying to Log out, Would you like to continue?',
-                [
-                  { text: 'Cancel', onPress: () => null },
-                  { text: 'Ok', onPress: () => handleLogout()}
-                ]
-              );
-            }}>
-              <Text>LogOut</Text>
+          <View className="w-full p-5 bg-white rounded-lg shadow-lg">
+            <AddTodo onAdd={fetchTodos} />
+            <Pressable
+              className="px-[20] py-[12] rounded-lg bg-red-600 shadow-sm w-full"
+              onPress={() => setModalVisible(false)}
+            >
+              <Text className="font-bold text-center text-white uppercase">
+                Cancel
+              </Text>
             </Pressable>
           </View>
         </View>
-        <View className="h-32 p-4 pl-5 mt-3 bg-blue-400 rounded-xl">
-          <Text className="mb-2 text-xl font-bold text-white">
-            Welcome, {userName}!
-          </Text>
-          <Text className="text-base font-semibold text-white">
-            Keep it up!! Complete your tasks. You almost their!
-          </Text>
-        </View>
-      </View>
-      <View className="max-w-md p-4 pt-1 pb-8 mx-auto">
-        <View className="">
-          <FlatList
-            data={todos}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-              <TodoItem
-                id={item.id}
-                task={item.task}
-                completed={item.completed}
-                onDelete={deleteTodo}
-                onUpdate={fetchTodos}
-                onToggleComplete={toggleComplete}
-              />
-            )}
-          />
-        </View>
-        <Pressable
-          className="absolute -bottom-10  w-16 p-4 bg-blue-400 rounded-full shadow-lg left-[315px]"
-          onPress={() => setModalVisible(true)}
-        >
-          <Text className="pl-2 text-2xl text-white ">+</Text>
-        </Pressable>
-        <Modal
-          animationType="slide"
-          visible={modalVisible}
-          presentationStyle="pageSheet"
-          onRequestClose={() => setModalVisible(false)}
-        >
-          <View className="items-center justify-center flex-1 bg-opacity-50">
-            <View className="mb-10">
-              <Text className="text-2xl font-bold">New Task</Text>
-            </View>
-            <View className="w-full p-5 bg-white rounded-lg shadow-lg">
-              <AddTodo onAdd={fetchTodos} />
-              <Pressable
-                className="px-[20] py-[12] rounded-lg bg-red-600 shadow-sm w-full"
-                onPress={() => setModalVisible(false)}
-              >
-                <Text className="font-bold text-center text-white uppercase">
-                  Cancel
-                </Text>
-              </Pressable>
-            </View>
-          </View>
-        </Modal>
-      </View>
+      </Modal>
     </View>
+  );
+
+
+  
+  return (
+
+    <FlatList
+    data={todos}
+    keyExtractor={(item) => item.id}
+    renderItem={({ item }) => (
+      <TodoItem
+        id={item.id}
+        task={item.task}
+        completed={item.completed}
+        onDelete={deleteTodo}
+        onUpdate={fetchTodos}
+        onToggleComplete={toggleComplete}
+        />
+      )}
+      ListHeaderComponent={renderHeader}
+      ListFooterComponent={renderFooter}
+  />
   );
 };
 
